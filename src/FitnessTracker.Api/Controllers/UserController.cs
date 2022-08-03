58f525b1c853c71ff72fe92efdab2eb020a803a5
JulianCodespoti <juliancodespoti@gmail.com>
2022-08-04 00:35:57 +1000

    added missing files:

---
+++ src/FitnessTracker.Api/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+using FitnessTracker.Contracts.Requests.Authorization;
+using FitnessTracker.Interfaces;
+using FitnessTracker.Models.Authorization;
+using FluentValidation;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessTracker.Api.Controllers;
+
+[ApiController]
+[Route("Users")]
+public class UserController : ControllerBase
+{
+    private readonly IAuthorizationHandler authorizationHandler;
+    private readonly ILogger logger;
+
+    public UserController(ILogger<UserController> logger,
+        IAuthorizationHandler authorizationHandler)
+    {
+        this.logger = logger;
+        this.authorizationHandler = authorizationHandler;
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request,
+        [FromServices] IValidator<LoginRequest> validator)
+    {
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+
+        var loginResponse = await authorizationHandler.LoginAsync(request.Adapt<LoginParameters>());
+        if (!loginResponse.IsSuccess) return BadRequest(loginResponse.Error);
+
+        return Ok(loginResponse.Value);
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request,
+        [FromServices] IValidator<RegisterRequest> validator)
+    {
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.Errors);
+        }
+
+        var registerResponse = await authorizationHandler.RegisterAsync(request.Adapt<RegistrationParameters>());
+        if (!registerResponse.IsSuccess)
+        {
+            return BadRequest(registerResponse.Error);
+        }
+
+        return Ok(registerResponse.Value);
+    }
+}
---
+++ src/FitnessTracker.Application/Authorization/UserHandler.cs
@@ -0,0 +1,69 @@
+﻿using FitnessTracker.Contracts.Responses.Authorization;
+using FitnessTracker.Interfaces;
+using FitnessTracker.Models.Authorization;
+using FitnessTracker.Models.Common;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FitnessTracker.Application.Authorization;
+
+public class UserHandler : IAuthorizationHandler
+{
+    private readonly IApplicationDbContext applicationDbContext;
+    private readonly ILogger logger;
+
+    public UserHandler(IApplicationDbContext applicationDbContext, ILogger<UserHandler> logger)
+    {
+        this.applicationDbContext = applicationDbContext;
+        this.logger = logger;
+    }
+
+    public async Task<Result<LoginResponse>> LoginAsync(LoginParameters loginParameters)
+    {
+        var user = await applicationDbContext.Users.FirstOrDefaultAsync(u => u.Email == loginParameters.Email);
+
+        if (user is null)
+        {
+            var message = $"User with email {loginParameters.Email} does not exist.";
+            logger.LogError(message);
+            return Result<LoginResponse>.Failure(message);
+        }
+
+        if (!user.Password.Equals(loginParameters.Password))
+        {
+            var message = $"Password for user with email {loginParameters.Email} is incorrect.";
+            logger.LogError(message);
+            return Result<LoginResponse>.Failure(message);
+        }
+
+        var response = new LoginResponse
+        {
+            User = user
+        };
+        return Result<LoginResponse>.Success(response);
+    }
+
+    public async Task<Result<RegisterResponse>> RegisterAsync(RegistrationParameters registrationParameters)
+    {
+        var userInDatabase =
+            await applicationDbContext.Users.FirstOrDefaultAsync(u => u.Email == registrationParameters.Email);
+
+        if (userInDatabase is not null)
+        {
+            var message = $"User with email {registrationParameters.Email} already exists.";
+            logger.LogError(message);
+            return Result<RegisterResponse>.Failure(message);
+        }
+
+        var user = registrationParameters.Adapt<User>();
+        await applicationDbContext.Users.AddAsync(user);
+        await applicationDbContext.SaveChangesAsync();
+
+        var response = new RegisterResponse
+        {
+            User = user
+        };
+        return Result<RegisterResponse>.Success(response);
+    }
+}
