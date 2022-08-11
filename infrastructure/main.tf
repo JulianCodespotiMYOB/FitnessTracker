52b948b9dbd225084e58f72c7d46fb2038ad7791
Dom <domcodespoti@gmail.com>
2022-08-11 23:30:50 +1000

    change conn string style

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,7 +24,7 @@ locals {
   }
 }
 
-variable "connection_string" {
+variable "CONNECTION_STRING" {
   type        = string
   description = "The DB connection string."
   sensitive = true
@@ -73,7 +73,7 @@ resource "aws_instance" "app_server" {
                 ACCOUNT_ID=`aws sts get-caller-identity --query "Account" --output text`
                 aws ecr get-login-password --region ${local.region} | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.${local.region}.amazonaws.com
 
-                export CONNECTION_STRING=${var.connection_string}
+                export CONNECTION_STRING=${var.CONNECTION_STRING}
                 docker pull $ACCOUNT_ID.dkr.ecr.${local.region}.amazonaws.com/${local.repository}:latest
                 docker run -e CONNECTION_STRING -p 80:80 $ACCOUNT_ID.dkr.ecr.${local.region}.amazonaws.com/${local.repository}:latest
                 EOT
