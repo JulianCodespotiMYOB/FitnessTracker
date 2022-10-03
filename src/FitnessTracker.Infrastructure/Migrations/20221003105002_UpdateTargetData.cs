94fb68ee117179f46d13235e72733c3796481beb
Julian Codespoti <julian.codespoti@myob.com>
2022-10-03 21:56:24 +1100

    add migration

---
+++ src/FitnessTracker.Infrastructure/Migrations/20221003105002_UpdateTargetData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTargetData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TargetDuration",
+                table: "Data");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TargetDuration",
+                table: "Data",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
