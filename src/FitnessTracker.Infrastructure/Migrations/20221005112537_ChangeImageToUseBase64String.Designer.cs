a1e37e35b3eb0edf0abdd170a9ca9652aec91aef
Dom <domcodespoti@gmail.com>
2022-10-05 22:25:51 +1100

    Change byte array to string

--- src/FitnessTracker.Infrastructure/Migrations/20221005112537_ChangeImageToUseBase64String.Designer.cs
+++ src/FitnessTracker.Infrastructure/Migrations/20221005112537_ChangeImageToUseBase64String.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FitnessTracker.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FitnessTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221005112537_ChangeImageToUseBase64String")]
+    partial class ChangeImageToUseBase64String
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/FitnessTracker.Models/Users/Image.cs
+++ src/FitnessTracker.Models/Users/Image.cs
@@ -3,7 +3,7 @@ namespace FitnessTracker.Models.Users;
 public class Image
 {
      public int Id { get; set; }
-     public byte[] Bytes { get; set; }
+     public string Bytes { get; set; }
      public string Name { get; set; }
      public string FileExtension { get; set; }
 }
